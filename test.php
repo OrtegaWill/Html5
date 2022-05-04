1999ee3edecaf093ce5281b61cf1127153da3294
Wilver <wilver.ortega@gmail.com>
2022-05-04 17:33:00 -0500

    Subido por wilver

--- test.php
+++ test.php
@@ -3,4 +3,5 @@ $denominador=10;
 $division= denominador / $numerador;
 $suma = $numerador + $denominador;
 $suma2=$division + $numerador;
-G|<p1>    Parraro 1 de prueba </p1>
+<p1>    Parraro 1 de prueba </p1>
+<p1> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam molestiae corrupti dolorum facere similique quae ducimus dolorem rerum, aliquid dicta ad quis porro reprehenderit ipsum possimus eos a exercitationem sequi!</p>
