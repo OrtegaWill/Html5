bcb3882658de615d0f7579f3d3f9218aa9416cde
Wilver <wilver.ortega@gmail.com>
2022-05-04 16:55:50 -0500

    fusion de master

--- test.php
+++ test.php
@@ -3,4 +3,4 @@ $denominador=10;
 $division= denominador / $numerador;
 $suma = $numerador + $denominador;
 $suma2=$division + $numerador;
-<p1>    Parraro 1 de prueba </p1>
+G|<p1>    Parraro 1 de prueba </p1>
